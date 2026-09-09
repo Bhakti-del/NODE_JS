0c7a0aa7a847f2c3ca30390d3e3be4e5860ecd29
Bhakti Takey <bhakti@Bhaktis-MacBook-Air.local>
2026-09-09 17:29:39 +0530

    Added exp1 and exp3

---
+++ exp3/demo.js
@@ -0,0 +1,31 @@
+console.log("My name is bhakti");
+alert("Bhakti Takey");
+
+console.log("My name is bhakti");
+j = "bhakti";
+console.log(j)
+
+var jahnavi = "typing";
+console.log(bhakti)
+
+var jahnavi = "executing";
+console.log(bhakti)
+
+let college = "BVRITH"
+console.log(college);
+
+college = "CSE"
+console.log(college);
+
+const age = 19;
+console.log(age);
+
+if (age>18) {
+    alert("Eligible to vote");
+}
+else 
+{
+    alert("Not eligible to vote");
+}
+document.write("Hello world");
+document.write("<h1>My name is Bhakti</h1>");
